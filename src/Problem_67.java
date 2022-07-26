969205066309f2d86194dcd983cd5cc9906455f0
KashGiannis34 <akashtechnion@gmail.com>
2022-07-26 10:27:53 -0400

    commit likeabossssssssss

---
+++ src/Problem_67.java
@@ -0,0 +1,34 @@
+//Maximum path sum II
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Problem_67 {
+    private static ArrayList<Integer>[] tri = triList();
+    public static void main(String[] args) {
+        for (int a = 98; a >= 0; a--)
+        {
+            for (int b = 0; b < tri[a].size(); b++)
+            {
+                tri[a].set(b, Math.max(tri[a].get(b)+tri[a+1].get(b), tri[a].get(b)+tri[a+1].get(b+1)));
+            }
+        }
+        System.out.println(tri[0].get(0));
+    }
+
+    static ArrayList<Integer>[] triList()
+    {
+        int count = 1;
+        ArrayList<Integer>[] nums = new ArrayList[100];
+        Scanner sc = new Scanner(Objects.requireNonNull(Problem_67.class.getResourceAsStream("/p067_triangle.txt")));
+        while (sc.hasNextLine())
+        {
+            nums[count-1] = IntStream.of(Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray()).boxed().collect(Collectors.toCollection(ArrayList::new));
+            count++;
+        }
+        return nums;
+    }
+}
---
+++ src/Problem_72.java
@@ -0,0 +1,55 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+// counting fractions
+public class Problem_72 {
+    public static void main(String[] args) {
+        System.out.println(f(1000000));
+    }
+
+    static int tot(int n)
+    {
+        double tVal = n;
+        ArrayList<Integer> factors = pFactors(n);
+        for (int factor: factors)
+            tVal *= (1-(1/(double)factor));
+        return (int) tVal;
+    }
+
+    static ArrayList<Integer> pFactors(int n)
+    {
+        ArrayList<Integer> factors = new ArrayList<>();
+        while (n > 1)
+        {
+            boolean prime = true;
+            for (int f = 2; f <= Math.sqrt(n); f++)
+            {
+                if (n%f == 0)
+                {
+                    n /= f;
+                    if (!factors.contains(f))
+                        factors.add(f);
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+            {
+                if (!factors.contains(n))
+                    factors.add(n);
+                return factors;
+            }
+        }
+        return factors;
+    }
+
+    static BigInteger f(int n)
+    {
+        BigInteger num = BigInteger.ZERO;
+        for (int t = 2; t <= n; t++)
+        {
+            num = num.add(BigInteger.valueOf(tot(t)));
+        }
+        return num;
+    }
+}
